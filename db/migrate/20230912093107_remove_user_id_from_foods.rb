45d7ccac94905267b2c3ce277a337b9fbed7b22c
Ankit <ankit26kushwa@gmail.com>
2023-09-12 19:47:22 +0530

    Remove unused schema

--- db/migrate/20230912093107_remove_user_id_from_foods.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserIdFromFoods < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :foods, :user_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2023_09_12_102948) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,16 +31,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_12_102948) do
     t.index ["user_id"], name: "index_inventories_on_user_id"
   end
 
-  create_table "recipes", force: :cascade do |t|
-    t.string "name"
-    t.string "preparation_time"
-    t.string "cooking_time"
-    t.string "description"
-    t.boolean "public"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-
   create_table "inventory_foods", force: :cascade do |t|
     t.integer "quantity"
     t.datetime "created_at", null: false
@@ -56,6 +45,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_12_102948) do
     t.datetime "updated_at", null: false
     t.bigint "food_id", null: false
     t.index ["food_id"], name: "index_recipe_foods_on_food_id"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.string "preparation_time"
+    t.string "cooking_time"
+    t.string "description"
+    t.boolean "public"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
