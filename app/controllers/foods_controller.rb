128709f8e612c5f2a9ac6b056b04615706b62dc2
forLoop94 <charlesuwem94@gmail.com>
2023-09-12 13:58:45 +0100

    Add create action to controller and modify routes.rb

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -5,6 +5,26 @@ class FoodsController < ApplicationController
   end
 
   def new
-    
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.create(food_params)
+    respond_to do |format|
+      format.html do
+        if @food.save
+          redirect_to foods_path, notice: 'food was successfully added.'
+        else
+          render :new
+          flash.now[:alert] = 'food could not be added. Please try again.'
+        end
+      end
+    end
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name,:measurement_unit, :price)
   end
 end
