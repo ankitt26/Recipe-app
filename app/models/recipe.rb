e9f793ad8fb328dfc06baa6945ae543ec34387e2
Ankit <ankit26kushwa@gmail.com>
2023-09-14 00:54:03 +0530

    create get_foods method in Recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,8 @@ class Recipe < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   has_many :recipe_foods
   has_many :foods, through: :recipe_foods
+
+  def get_foods
+    self.foods
+  end
 end
