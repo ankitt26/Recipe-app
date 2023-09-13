57d8b709744cb2c33b296e2fdf078fb30b29890b
Reggeditt <tkodjosarso@gmail.com>
2023-09-13 03:42:56 +0000

    update foods model, controller and views

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,6 +1,22 @@
 class FoodsController < ApplicationController
   def index
-    @user = current_user
     @foods = Food.all
+  end
+
+  def show
+    @food = Food.all
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.create(food_params)
+    redirect_to foods_path
+  end
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price)
   end
 end
