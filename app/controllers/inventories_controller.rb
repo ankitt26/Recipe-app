6cad6f29ecc26b2a8e51e723b331f0603f26f2f7
Reggeditt <tkodjosarso@gmail.com>
2023-09-13 03:49:26 +0000

    update inventories controller and views

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -7,7 +7,9 @@ class InventoriesController < ApplicationController
   end
 
   # GET /inventories/1 or /inventories/1.json
-  def show; end
+  def show
+    @inventory_foods = InventoryFood.all
+  end
 
   # GET /inventories/new
   def new
