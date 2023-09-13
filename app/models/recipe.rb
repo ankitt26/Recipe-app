9f39a9228e2c2a3e304da4834c5cf7581d0931b1
Ankit <ankit26kushwa@gmail.com>
2023-09-14 01:14:59 +0530

    rename get_food to food_in_recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,8 @@ class Recipe < ApplicationRecord
   has_many :recipe_foods
   has_many :foods, through: :recipe_foods
 
-  def get_foods
-    self.foods
+  def foods_in_recipe
+    foods
   end
+
 end
