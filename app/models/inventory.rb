e4b41366229d20204f587ef8072cf4fcc17896cf
forLoop94 <charlesuwem94@gmail.com>
2023-09-14 13:11:47 +0100

    Style inventory index page

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,3 +1,4 @@
 class Inventory < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  has_many :inventory_foods, class_name: 'InventoryFood', dependent: :destroy
 end
