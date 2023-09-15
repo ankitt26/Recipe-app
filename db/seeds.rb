43c0bf5f6b0ad6dd6e7e9a248c894c902be37008
Ankit <ankit26kushwa@gmail.com>
2023-09-15 14:55:34 +0530

    Add submit_food path

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+RecipeFood.create(quantity:1 , food_id:9, recipe_id:6)
