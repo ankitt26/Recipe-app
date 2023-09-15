d447373b4307126bed15bd70d1eb88cd37231c18
Ankit <ankit26kushwa@gmail.com>
2023-09-15 19:38:03 +0530

    Add dependent destroy in models

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,6 @@
 class Food < ApplicationRecord
-  has_many :inventory_foods
+  has_many :inventory_foods, dependent: :destroy
 
-  has_many :recipe_foods
+  has_many :recipe_foods, dependent: :destroy
   has_many :recipes, through: :recipe_foods
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
-  has_many :recipe_foods
+  has_many :recipe_foods , dependent: :destroy
   has_many :foods, through: :recipe_foods
 
   def foods_in_recipe
