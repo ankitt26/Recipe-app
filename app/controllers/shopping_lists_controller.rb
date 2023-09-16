a1f9acf94ecde66d4bf33da1d9ac0383fe1964fa
Ankit <ankit26kushwa@gmail.com>
2023-09-16 13:29:42 +0530

    Render general_shopping list

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,13 +1,11 @@
 class ShoppingListsController < ApplicationController
-    def index
-        @recipe = Recipe.all
-    end
+  def index
+    @recipe = Recipe.all
+  end
 
-    def shopping_list 
-        recipe_id = params[:recipe_id]
-        inventory_id = params[:inventory_id]
-      
-        flash[:alert] = "Recipe ID: #{recipe_id}, Inventory ID: #{inventory_id}"
-      end
-      
+  def shopping_list
+    inventory_id = params[:selected_inventory]
+    recipe_id_param = params[:recipe_id_param]
+    flash[:alert] = "Recipe ID: #{recipe_id_param}, Inventory ID: #{inventory_id}"
+  end
 end
