3cad7519c4a2496b2b0cc7a140f1ef7ca81aa3dc
Ankit <ankit26kushwa@gmail.com>
2023-09-16 15:55:05 +0530

    Fix linters

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -5,9 +5,9 @@ class ShoppingListsController < ApplicationController
   end
 
   def shopping_list
-    inventory_id = params[:selected_inventory]
+    params[:selected_inventory]
     recipe_id_param = params[:recipe_id_param]
-    
+
     @recipe_g = Recipe.find(recipe_id_param)
     @inventory_g = Inventory.find(params[:selected_inventory])
 
@@ -19,33 +19,31 @@ class ShoppingListsController < ApplicationController
     recipe_food = recipe_foods.find_by(food_id: food.id)
     recipe_food&.quantity
   end
-  
-    private
-    def generate_shopping_list(recipe, inventory)
-      # Initialize variables to store missing items and total price
-      missing_items = []
-      total_price = 0
-    
-      # Loop through each food item in the recipe
-      recipe.foods.each do |food|
-        inventory_item = inventory.foods.find_by(id: food.id)
-    
-        # Check if the food item is not found in the inventory or if the quantity is insufficient
-        if inventory_item.nil?
-          missing_items << food
-          total_price += food.price
-        elsif inventory_item.quantity < 1 # Assuming 1 unit per recipe ingredient
-          missing_items << food
-          total_price += (1 - inventory_item.quantity) * food.price
-        end
+
+  private
+
+  def generate_shopping_list(recipe, inventory)
+    # Initialize variables to store missing items and total price
+    missing_items = []
+    total_price = 0
+
+    # Loop through each food item in the recipe
+    recipe.foods.each do |food|
+      inventory_item = inventory.foods.find_by(id: food.id)
+
+      # Check if the food item is not found in the inventory or if the quantity is insufficient
+      if inventory_item.nil?
+        missing_items << food
+        total_price += food.price
+      elsif inventory_item.quantity < 1 # Assuming 1 unit per recipe ingredient
+        missing_items << food
+        total_price += (1 - inventory_item.quantity) * food.price
       end
-    
-      # Render or pass the missing_items and total_price to your view
-      # For example:
-      @missing_items = missing_items
-      @total_price = total_price
     end
-    
-   
-  
+
+    # Render or pass the missing_items and total_price to your view
+    # For example:
+    @missing_items = missing_items
+    @total_price = total_price
+  end
 end
