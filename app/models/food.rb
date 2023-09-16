109d981211dd771bf5cb682c24bda2d60954beb9
Ankit <ankit26kushwa@gmail.com>
2023-09-16 15:50:19 +0530

    update assosiation

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,6 @@
 class Food < ApplicationRecord
   has_many :inventory_foods, dependent: :destroy
+  has_many :inventories, through: :inventory_foods
 
   has_many :recipe_foods, dependent: :destroy
   has_many :recipes, through: :recipe_foods
